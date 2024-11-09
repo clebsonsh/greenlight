1525d432644e6d23dbc61c62fa7439c64cb09b2a
Clebson Moura <clebson.sh@gmail.com>
2024-11-09 12:00:37 -0300

    Hiding struct fields in the JSON object

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,10 +4,10 @@ import "time"
 
 type Movie struct {
 	CreatedAt time.Time `json:"id"`
-	Title     string    `json:"created_at"`
+	Title     string    `json:"-"`
 	Genres    []string  `json:"title"`
-	ID        int64     `json:"year"`
-	Year      int32     `json:"runtime"`
-	Runtime   int32     `json:"genres"`
+	ID        int64     `json:"year,omitempty"`
+	Year      int32     `json:"runtime,omitempty"`
+	Runtime   int32     `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 }
