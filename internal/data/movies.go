e289bfe1ca2c8cbd7d777b751261883369f2606e
Clebson Moura <clebson.sh@gmail.com>
2024-11-09 12:30:48 -0300

    Formatting and Enveloping Responses

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,11 +3,11 @@ package data
 import "time"
 
 type Movie struct {
-	CreatedAt time.Time `json:"id"`
-	Title     string    `json:"-"`
-	Genres    []string  `json:"title"`
-	ID        int64     `json:"year,omitempty"`
-	Year      int32     `json:"runtime,omitempty"`
-	Runtime   int32     `json:"genres,omitempty"`
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 }
