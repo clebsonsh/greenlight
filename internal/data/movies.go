3c0d92490a7de3006fc1a500fa1c677f4d5a8197
Clebson Moura <clebson.sh@gmail.com>
2024-11-09 11:45:06 -0300

    Changing keys in the JSON object

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,11 +3,11 @@ package data
 import "time"
 
 type Movie struct {
-	CreatedAt time.Time
-	Title     string
-	Genres    []string
-	ID        int64
-	Year      int32
-	Runtime   int32
-	Version   int32
+	CreatedAt time.Time `json:"id"`
+	Title     string    `json:"created_at"`
+	Genres    []string  `json:"title"`
+	ID        int64     `json:"year"`
+	Year      int32     `json:"runtime"`
+	Runtime   int32     `json:"genres"`
+	Version   int32     `json:"version"`
 }
