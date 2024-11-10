abe1475b99894c132656af648409110562f0839c
Clebson Moura <clebson.sh@gmail.com>
2024-11-10 08:44:10 -0300

    Creating a New Movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -12,9 +12,9 @@ import (
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		Title   string       `json:"title"`
+		Genres  []string     `json:"genres"`
 		Year    int32        `json:"year"`
 		Runtime data.Runtime `json:"runtime"`
-		Genres  []string     `json:"genres"`
 	}
 
 	err := app.readJSON(w, r, &input)
@@ -37,7 +37,19 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	err = app.models.Movies.Insert(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
